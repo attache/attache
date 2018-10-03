00a78a1848f30a5dcf1f654622093ffd5289d469
Jacob McCollum <jacob.r.mccollum@gmail.com>
2018-10-03 00:11:18 -0400

    provide top-level functions for accessing DB and ViewCache instances

--- global_services.go
+++ global_services.go
@@ -11,6 +11,21 @@ var (
 	gsSessions    = sessions.NewCookieStore()
 )
 
+// ViewCacheFor will return the cached ViewCache or a
+// newly initialized ViewCache for the given ViewConfig.
+// Any error encountered while initializing the new ViewCahce
+// is returned.
+func ViewCacheFor(conf ViewConfig) (ViewCache, error) {
+	return gsCache.viewsFor(conf)
+}
+
+// DBFor will returh the cached DB or a newly initialized DB
+// for the given DBConfig. Any error encountered while
+// initializing a new DB is returned.
+func DBFor(conf DBConfig) (DB, error) {
+	return gsCache.dbFor(conf)
+}
+
 func init() {
 	gsFormDecoder.IgnoreUnknownKeys(true)
 }
