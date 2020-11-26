487d11886df75c71395f22f291bebe4e3f1d0fd5
Jacob McCollum <jacob.r.mccollum@gmail.com>
2020-11-25 22:35:07 -0500

    switch to taskfile
    prefer taskfile.yml tasks over bash scripts

--- build_cli.bash
+++
@@ -1,8 +0,0 @@
-pushd ./cmd/attache/internal/cmd_gen >> /dev/null
-go generate
-popd >> /dev/null
-pushd ./cmd/attache/internal/cmd_new >> /dev/null
-go generate
-popd >> /dev/null
-
-go install ./cmd/attache
